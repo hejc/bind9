4b1ca4450f9db97f2ed3fae1feee3396efc1a7bf
Mark Andrews <marka@isc.org>
2021-06-23 19:50:49 +1000

    Only test digest lengths if the scheme is known
    The test zones in RFC 8976 refused to load because one of the
    ZONEMD records is too short for the specified digest type.  This
    change allow the test zone to load.  There is an open errata
    covering this.  https://www.rfc-editor.org/errata/eid6425

--- lib/dns/rdata/generic/zonemd_63.h
+++ lib/dns/rdata/generic/zonemd_63.h
@@ -11,6 +11,9 @@
 
 #pragma once
 
+/* Known scheme type(s). */
+#define DNS_ZONEMD_SCHEME_SIMPLE (1)
+
 /* Known digest type(s). */
 #define DNS_ZONEMD_DIGEST_SHA384 (1)
 #define DNS_ZONEMD_DIGEST_SHA512 (2)
