oss-mirror/isc-bind9

Update ZONEMD records as part of UPDATE processing
dns_update_zonemd() has been added update ZONEMD records.

dns_update_signaturesinc() has been extended to update (via
dns_update_zonemd) and sign ZONEMD records.

bin/tests/system/nsupdate/ns3/sign.sh
@@ -43,3 +43,14 @@ keyname2=`$KEYGEN -q -a RSASHA256 -3 $zone`
 cat $infile $keyname1.key $keyname2.key >$zonefile
 
 $SIGNER -A -3 - -P -o $zone -k $keyname1 $zonefile $keyname2 > /dev/null
+
+zone=zonemd.test.
+infile=zonemd.test.db.in
+zonefile=zonemd.test.db
+
+keyname1=`$KEYGEN -q -a RSASHA1 -b 1024 -n zone -f KSK $zone`
+keyname2=`$KEYGEN -q -a RSASHA1 -b 1024 -n zone $zone`
+
+cat $infile $keyname1.key $keyname2.key >$zonefile
+
+$SIGNER -P -o $zone -k $keyname1 $zonefile $keyname2 > /dev/null

hejc/bind9

bin/tests/system/nsupdate/ns3/sign.sh
@@ -46,3 +46,14 @@ $SIGNER -A -3 - -P -o $zone -k $keyname1 $zonefile $keyname2 > /dev/null
 
 # Just copy multisigner.db.in because it is signed with dnssec-policy.
 cp multisigner.test.db.in multisigner.test.db
+
+zone=zonemd.test.
+infile=zonemd.test.db.in
+zonefile=zonemd.test.db
+
+keyname1=`$KEYGEN -q -a RSASHA1 -b 1024 -n zone -f KSK $zone`
+keyname2=`$KEYGEN -q -a RSASHA1 -b 1024 -n zone $zone`
+
+cat $infile $keyname1.key $keyname2.key >$zonefile
+
+$SIGNER -z -o $zone -k $keyname1 $zonefile $keyname2 > /dev/null